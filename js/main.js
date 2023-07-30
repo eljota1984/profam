bf6c4bbca7916571849993da593f6870b9dc36e9
eljota1984 <juan.orellana1984@gmail.com>
2023-07-30 19:02:13 -0400

    actualizacion

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const nav = document.querySelector("#nav")
 const abrir = document.querySelector("#abrir")
 const cerrar = document.querySelector("#cerrar")
 
-abrir.addEventListener("click", () =>{
+abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-} )
+})
 
-cerrar.addEventListener("click", () =>{
+cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-} )
+})
