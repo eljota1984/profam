8466ac116d6330699f8628d4946dcc80f024c11b
eljota1984 <juan.orellana1984@gmail.com>
2023-07-30 23:12:05 -0400

    actualizacion de contacto

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const nav = document.querySelector("#nav")
-const abrir = document.querySelector("#abrir")
-const cerrar = document.querySelector("#cerrar")
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+    nav.classList.add("visible")
+});
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+    nav.classList.remove("visible")
+});
